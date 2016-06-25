a9f6001a6e7af95ef3a34d642309aab376cd87de
draco607 <draco425@gmail.com>
2016-06-25 17:20:25 -0400

    Config
    Test

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "driver"    =>"pgsql",
+    "host"      =>"164.132.176.38",
+    "user"      =>"postgres",
+    "pass"      =>"19740533",
+    "database"  =>"sigecopadf",
+    "charset"   =>"utf8",
+);
+?>
